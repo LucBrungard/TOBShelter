ac01907c3f7fb3792519d03e18ee6f88285d83ea
LucBrungard <luc.brungard@laposte.net>
2023-04-08 12:06:04 +0200

    refactor: :recycle: renamed some Regex utils

--- TOBShelter/utils/RegexUtil.cs
+++ TOBShelter/utils/RegexUtil.cs
@@ -6,8 +6,8 @@ namespace TOBShelter.Utils
     {
         public readonly static Regex RegexNom = new Regex("^[A-Za-zÀ-ÖØ-öø-ÿ]+([-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$");
         public readonly static Regex RegexAdresse = new Regex("^[A-Za-z0-9\\s]*$"); // TODO trouver une bonne regex d'adresse
-        public readonly static Regex RegexTel = new Regex("^0[1-9][0-9]{8}$");
-        public readonly static Regex RegexCodePostal = new Regex("^[0-9]{5}$");
+        public readonly static Regex RegexPhoneNumber = new Regex("^0[1-9][0-9]{8}$");
+        public readonly static Regex RegexPostalCode = new Regex("^((01)|([1-9][0-9]))[0-9]{3}$");
         public readonly static Regex RegexEmail = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
     }
 }
