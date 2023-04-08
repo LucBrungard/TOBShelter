c79266446bbdca505c696d475b571034d11771a8
LucBrungard <luc.brungard@laposte.net>
2023-04-08 12:09:36 +0200

    feat: :sparkles: create PersonDTO.cs

---
+++ TOBShelter/types/dto/PersonDTO.cs
@@ -0,0 +1,31 @@
+﻿using TOBShelter.Types.Base;
+
+namespace TOBShelter.Types.Dto
+{
+    internal class PersonDetailsDTO
+    {
+        internal IdentityTitle Title { get; set; }
+        internal string Name { get; set; }
+        internal string FirstName { get; set; }
+        internal string Mobile { get; set; }
+        internal string Home { get; set; }
+        internal string Email { get; set; }
+        internal string NumRoute { get; set; }
+        internal RouteType RouteType { get; set; }
+        internal string RouteName { get; set; }
+        internal string PostalCode { get; set; }
+        internal string City { get; set; }
+    }
+
+    internal class PersonEditDTO
+    {
+        internal string Mobile { get; set; }
+        internal string Home { get; set; }
+        internal string Email { get; set; }
+        internal string NumRoute { get; set; }
+        internal RouteType RouteType { get; set; }
+        internal string RouteName { get; set; }
+        internal string PostalCode { get; set; }
+        internal string City { get; set; }
+    }
+}
