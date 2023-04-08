e4905f91f74b0b8aab2b5d0578a2f66de7791238
LucBrungard <luc.brungard@laposte.net>
2023-04-08 12:08:56 +0200

    feat: :sparkles: create DbUtil.cs

---
+++ TOBShelter/utils/DbUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TOBShelter.utils
+{
+    internal static class DbUtil
+    {
+        internal static bool CheckSqlInjection(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+
+            string[] subs = value.Split(' ');
+            if (subs.Length > 1)
+                return true;
+
+            if (Regex.IsMatch(value, "^[0-9]+$"))
+                return true;
+
+            return false;
+        }
+    }
+}
