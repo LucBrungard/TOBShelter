5009bed3e4ad4db1c7b3f3fd756dc77c01386161
Claire THIL <thil.claire07@gmail.com>
2023-04-25 01:54:08 +0200

    Ajout GMap au projet + début interface ajout enquete

---
+++ TOBShelter/AddInvestigation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TOBShelter
+{
+    public partial class AddInvestigation : Form
+    {
+        public AddInvestigation()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
